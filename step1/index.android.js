6da00d9552b96ed7004e1d7cff40d59dadada4c6
soulmachine <soulmachine@gmail.com>
2017-01-29 06:43:56 -0800

    renamed step1 to Step1

--- step1/index.android.js
+++ step1/index.android.js
@@ -12,7 +12,7 @@ import {
   View
 } from 'react-native';
 
-export default class step1 extends Component {
+export default class Step1 extends Component {
   render() {
     return (
       <View style={styles.container}>
@@ -50,4 +50,4 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('step1', () => step1);
+AppRegistry.registerComponent('Step1', () => Step1);
--- step1/ios/step1Tests/step1Tests.m
+++ step1/ios/step1Tests/step1Tests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface step1Tests : XCTestCase
+@interface Step1Tests : XCTestCase
 
 @end
 
-@implementation step1Tests
+@implementation Step1Tests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
